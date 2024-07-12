1a7e0f384c1c4046799bf706a9aa12fb1c27cfb5
ibletz <ibletz@gmail.com>
2024-07-12 10:53:03 -0700

    Add Food and randomize position.

--- main..cpp
+++ main..cpp
@@ -8,14 +8,52 @@
 Color green{ 173,204,96,255 };
 Color darkGreen{ 43,51,24,255 };
 
+// define size of game grid
+int cellSize{ 30 };
+int cellCount{ 25 };
+
+class Food
+{
+public:
+
+	// position in cells
+	Vector2 position{};
+	Texture2D texture{};
+	
+	Food()
+	{
+		Image image{ LoadImage("graphics/food.png") };
+		texture = LoadTextureFromImage(image);
+		UnloadImage(image);
+		position = GenerateRandomPos();
+	}
+
+	~Food()
+	{
+		UnloadTexture(texture);
+	}
+
+	void Draw()
+	{
+		DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+	}
+
+	// randomly position the food
+	Vector2 GenerateRandomPos()
+	{
+		float x = GetRandomValue(0, cellCount - 1);
+		float y = GetRandomValue(0, cellCount - 1);
+		return Vector2{ x, y };
+	}
+};
+
 int main()
 {
 	std::cout << "Starting the game..." << '\n';
 
-	const int window_width(750);
-	const int window_height(750);
+	InitWindow(cellSize * cellCount, cellSize * cellCount, "raylib Snake");
 
-	InitWindow(window_width, window_height, "raylib Snake");
+	Food food{};
 
 	SetTargetFPS(60);
 	/*----------------------------------------------------*/
@@ -25,9 +63,11 @@ int main()
 	{
 		BeginDrawing();
 		ClearBackground(green);
-	
 		
-		
+		food.Draw();
+
+
+
 		EndDrawing();
 	}
 
